4569390cdac0e8553ae7f06f3885556e59648b36
rjohnson318 <8972701+rjohnson318@users.noreply.github.com>
2023-07-13 17:24:13 -0500

    Delete environments/dev/hub directory

--- environments/dev/hub/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "diagstoragetestsweepai" {
-  name                     = "diagstoragetestsweepai"
-  resource_group_name      = azurerm_resource_group.dev.name
-  location                 = azurerm_resource_group.dev.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
